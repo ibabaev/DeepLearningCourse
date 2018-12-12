49806e6fd137a69344d6f2c6ff0026868f4ab702
Daniil Privalov <iconbitpro@gmail.com>
2018-12-12 22:16:41 +0300

    lab2 updated, added report, test cases, new models

--- lab2/mnist_load.py
+++ lab2/mnist_load.py
@@ -3,6 +3,8 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.utils import np_utils
 
+print('MNIST test')
+
 #num of classes
 nb_classes = 10
 
